4293fac930e1d6c6fbe4789cd245fe98dcc9599f
Jader Santiago Nieves <jsnieves39@ucatolica.edu.co>
2026-03-25 00:16:22 -0500

    Ejercicio de Domino(Usando map y split)

--- pila_domino.py
+++ pila_domino.py
@@ -1,4 +1,10 @@
-m, n = map(int, input().split())
+m, n = map(int, input().split()) # split() separa texto 
+
+""" map() aplica una función a cada elemento de un iterable ((map(funcion, iterable)))
+ funcion → lo que quieres hacer (ej: int, str, abs)
+iterable → lista, split, etc."""
 
 print((m * n) // 2)
+
+
 
